b107a43936b3bfb24f5dc84945260a63ff807dfd
Shriya-1807 <shriyasdeo06@gmail.com>
2025-07-30 01:35:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -130,9 +130,10 @@ def process_video_with_yolo_deepsort(video_path, output_path, weights_path, skip
     if not cap.isOpened():
         st.error(" Could not open the uploaded video. Please try with a different .mp4 file.")
         return None
-    fps = cap.get(cv2.CAP_PROP_FPS)
+    
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
 
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))

a02447ef2744ef7188aca16186662fa45d809df9
Shriya-1807 <shriyasdeo06@gmail.com>
2025-07-30 01:29:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -128,8 +128,8 @@ def process_video_with_yolo_deepsort(video_path, output_path, weights_path, skip
     tracker = DeepSort(max_age=10)
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
-    st.error(" Could not open the uploaded video. Please try with a different .mp4 file.")
-    return None
+        st.error(" Could not open the uploaded video. Please try with a different .mp4 file.")
+        return None
     fps = cap.get(cv2.CAP_PROP_FPS)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))

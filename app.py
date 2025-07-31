f94b0084a6d462b8a43d22667abe902ba2ec4d9b
Shriya-1807 <shriyasdeo06@gmail.com>
2025-07-31 14:30:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def run_sahi_yolo_inference(image_pil, model_path, conf):
         if not os.path.exists(result_img_path):
             st.error(f"Export failed: File not created at {result_img_path}")
         else:
-            st.success(f"Export succeeded: File created at {result_img_path}"
+            st.success(f"Export succeeded: File created at {result_img_path}")
 
         
         
